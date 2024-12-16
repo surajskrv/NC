3e46355f5aa255824969880b316e165fc03df78f
Suraj Kumar <24dp1000038@ds.study.iitm.ac.in>
2024-12-16 15:42:35 +0530

    mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ import { ENV_VARS } from "./envVars.js";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+		mongoUri=ENV_VARS.MONGO_URI
+		const conn = await mongoose.connect(mongoUri);
 		console.log("MongoDB connected: " + conn.connection.host);
 	} catch (error) {
 		console.error("Error connecting to MONGODB: " + error.message);
