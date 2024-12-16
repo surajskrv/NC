d371f71a4e53144a580a63b419ca8d812413f459
Suraj Kumar <24dp1000038@ds.study.iitm.ac.in>
2024-12-16 13:54:22 +0530

    commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,15 +5,15 @@ const Footer = () => {
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
 					Built by{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/surajskrv'
 						target='_blank'
 						className='font-medium underline underline-offset-4'
 					>
-						you
+						Sam
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/surajskrv'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
