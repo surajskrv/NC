c5236572f074ec2204759e9c7fede4a72f6c18dd
Suraj Kumar <24dp1000038@ds.study.iitm.ac.in>
2024-12-17 14:19:08 +0530

    commit changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,11 @@ import { ENV_VARS } from "./envVars.js";
 
 export const connectDB = async () => {
 	try {
-		mongoUri=ENV_VARS.MONGO_URI
+		let mongoUri=ENV_VARS.MONGO_URI
 		const conn = await mongoose.connect(mongoUri);
 		console.log("MongoDB connected: " + conn.connection.host);
 	} catch (error) {
 		console.error("Error connecting to MONGODB: " + error.message);
-		process.exit(1); // 1 means there was an error, 0 means success
+		process.exit(1);
 	}
 };
